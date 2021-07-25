79e88c60bc897fccbbae2c1a209735443cd2e09f
gaku13 <gaku@bulbcorp.jp>
2021-07-25 15:31:11 +0900

    fix meta desc

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ module.exports = {
       }, {
         hid: 'description',
         name: 'description',
-        content: '持続可能な資産運用を誠実にサポートする、Double O Capital Ltd.のwebサイトです。'
+        content: '持続可能な資産運用を誠実にサポートする、Double O Capital Ltd.のwebサイトです。一生涯マンツーマン型で最新のマネートレンドから継続的な価値ある提案をおこないます。'
       }, {
         hid: 'og:type',
         property: 'og:type',
